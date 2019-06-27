fa94143a034a31c8d525f03c49f8d064aa10234b
Jesseliz <choque101108@gmail.com>
2019-06-27 17:59:41 -0500

    span para mensajes de error login

--- src/main.js
+++ src/main.js
@@ -1,1 +1,34 @@
 /* Manejo del DOM */
+const btnLogin = document.getElementById('btn-login');
+
+btnLogin.addEventListener('click', (evento) => {
+    evento.preventDefault();
+    let user = document.getElementById('user');
+    let password = document.getElementById('password');
+    const emptyUser = document.getElementById('empty-user');
+    const emptyPsw = document.getElementById('empty-psw');
+    const errorLogin = document.getElementById('error-login');
+    if (user.value !== '' && password.value !== '') {
+        if (user.value === 'LABORATORIA' && password.value === 'LABORATORIA') {
+            // paglogin.classList.add('hide');
+            // secHeader.classList.add('hide');
+            // pagCifrado.classList.remove('hide');
+            alert('bien');
+        }
+        else {
+            errorLogin.innerHTML = 'Usuario o contraseña incorrectas';
+        }
+    }
+    else {
+        if (user.value === '') {
+            emptyUser.innerHTML = 'Ingrese el usuario';
+            user.focus();
+        }
+        else {
+            emptyPsw.innerHTML = 'Ingrese la contraseña';
+            password.focus();
+        }
+
+
+    }
+})
