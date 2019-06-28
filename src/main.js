ea29857d84491bd38b0a0d317aea4f48cd9ea933
marcelimxo <marcelimrondon@gmail.com>
2019-06-28 08:04:13 -0500

    Limpiar en los inputs despues del error

--- src/main.js
+++ src/main.js
@@ -1,34 +1,31 @@
 /* Manejo del DOM */
-const btnLogin = document.getElementById('btn-login');
+const btnLogin = document.getElementById("btn-login");
 
-btnLogin.addEventListener('click', (evento) => {
-    evento.preventDefault();
-    let user = document.getElementById('user');
-    let password = document.getElementById('password');
-    const emptyUser = document.getElementById('empty-user');
-    const emptyPsw = document.getElementById('empty-psw');
-    const errorLogin = document.getElementById('error-login');
-    if (user.value !== '' && password.value !== '') {
-        if (user.value === 'LABORATORIA' && password.value === 'LABORATORIA') {
-            // paglogin.classList.add('hide');
-            // secHeader.classList.add('hide');
-            // pagCifrado.classList.remove('hide');
-            alert('bien');
-        }
-        else {
-            errorLogin.innerHTML = 'Usuario o contraseña incorrectas';
-        }
+btnLogin.addEventListener("click", evento => {
+  evento.preventDefault();
+  let user = document.getElementById("user");
+  let password = document.getElementById("password");
+  const emptyUser = document.getElementById("empty-user");
+  const emptyPsw = document.getElementById("empty-psw");
+  const errorLogin = document.getElementById("error-login");
+  if (user.value !== "" && password.value !== "") {
+    if (user.value === "LABORATORIA" && password.value === "LABORATORIA") {
+      // paglogin.classList.add('hide');
+      // secHeader.classList.add('hide');
+      // pagCifrado.classList.remove('hide');
+      alert("bien");
+    } else {
+      errorLogin.innerHTML = "Usuario o contraseña incorrectas";
+      emptyUser.innerHTML = "";
+      emptyPsw.innerHTML = "";
     }
-    else {
-        if (user.value === '') {
-            emptyUser.innerHTML = 'Ingrese el usuario';
-            user.focus();
-        }
-        else {
-            emptyPsw.innerHTML = 'Ingrese la contraseña';
-            password.focus();
-        }
-
-
+  } else {
+    if (user.value === "") {
+      emptyUser.innerHTML = "Ingrese el usuario";
+      user.focus();
+    } else {
+      emptyPsw.innerHTML = "Ingrese la contraseña";
+      password.focus();
     }
-})
+  }
+});
