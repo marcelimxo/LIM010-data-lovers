b58c3c3f7ac0e836c0c9995cdaab298c9d5849ed
Jesseliz <choque101108@gmail.com>
2019-07-12 08:35:25 -0500

    agregando tests de la funcion curated data

--- test/data.spec.js
+++ test/data.spec.js
@@ -3,6 +3,60 @@ global.assert = require('chai').assert;
 require('../src/data');
 require('./data.spec.js');
 
+const entrada1 = {
+  LeeSin: {
+    version: '6.24.1',
+    id: 'LeeSin',
+    key: '64',
+    name: 'Lee Sin',
+    title: 'the Blind Monk',
+    img:
+      'https://www.masterypoints.com/assets/img/lol/champion_icons/LeeSin.png',
+    splash:
+      'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/_0.jpg',
+    blurb:
+      'As a young teen, Lee Sin was intent on becoming a summoner. His will and dedication were unmatched by any of his peers, and his skill drew the attention of Reginald Ashram, the League\'s High Councilor at the time. While studying at the Arcanum Majoris,...',
+    info: {
+      attack: 8,
+      defense: 5,
+      magic: 3,
+      difficulty: 6
+    },
+    image: {
+      full: 'LeeSin.png',
+      sprite: 'champion1.png',
+      group: 'champion',
+      x: 288,
+      y: 96,
+      w: 48,
+      h: 48
+    },
+    tags: ['Fighter', 'Assassin'],
+    partype: 'Energy',
+    stats: {
+      hp: 570.8,
+      hpperlevel: 85,
+      mp: 200,
+      mpperlevel: 0,
+      movespeed: 350,
+      armor: 24.216,
+      armorperlevel: 3.7,
+      spellblock: 32.1,
+      spellblockperlevel: 1.25,
+      attackrange: 125,
+      hpregen: 7.425,
+      hpregenperlevel: 0.7,
+      mpregen: 50,
+      mpregenperlevel: 0,
+      crit: 0,
+      critperlevel: 0,
+      attackdamage: 61.176,
+      attackdamageperlevel: 3.2,
+      attackspeedoffset: -0.04,
+      attackspeedperlevel: 3
+    }
+  }
+};
 
 describe('loginValidate', () => {
   it('debería ser una función', () => {
@@ -25,17 +79,23 @@ describe('loginValidate', () => {
 
 describe('dataCurated', () => {
   it('debería ser una función', () => {
-    assert.equal(typeof dataCurated, 'function');
+    expect(typeof dataCurated).toBe('function');
   });
 
   it('debería devolver un nuevo objeto', () => {
-    // assert.equal(typeof dataCurated(,), 'function');
+    const entrada2 = {
+      LeeSin: 'img/splash/LeeSin_Splash_11.jpg'
+    };
+    expect(typeof dataCurated(entrada1, entrada2)).toBe('object');
   });
 });
 
 describe('selectedData', () => {
   it('debería ser una función', () => {
     expect(typeof selectedData).toBe('function');
+  });
+  it('debería ser una función', () => {
+    expect(typeof selectedData(entrada1)).toBe('object');
   });
 });
 
